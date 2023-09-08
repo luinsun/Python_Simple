bea6a0ee7f9545ae3773fc7b23c59ea4e45ac585
luinsun <135595841+luinsun@users.noreply.github.com>
2023-09-08 14:47:44 +0900

    Add python_01_variable

---
+++ lecture/python_01_variable.py
@@ -0,0 +1,11 @@
+#  주석(comment) : 메모, 실행 X
+
+# 글씨체: D2Coding
+# 1. 출력 함수(print)
+# - ()안의 값을 출력
+# - 변수값 확이 용도로 많이 사용
+print("Hello Python")
+
+# 문자열 타입(string Type)
+# Python: ' ' or " " → String
+# - C , Java: ' '(char), " "(Srintg)
