f2b5c6cf1b6f65182251d981fc5fac15a022dee2
luinsun <135595841+luinsun@users.noreply.github.com>
2023-11-24 14:21:10 +0900

    update prohect_daum_movie

--- Python_Simple-main/project_daum_movie/main.py
+++ Python_Simple-main/project_daum_movie/main.py
@@ -15,6 +15,7 @@
 #    - URL: https://sites.google.com/chromeium.org/driver/
 #   2. 실시간 다운로드
 
+from db.movie_dao import add_review
 from datetime import datetime, timedelta
 import math
 import re
@@ -112,3 +113,13 @@ for item in review_list:
         # 4) 계산된 등록일자 날짜 포매 변경(다음 영화 리뷰 날짜 포맷)
         review_date = review_date.strftime("%Y. %m. %d. %H:%M")
     print(f"   - 날짜: {review_date}")
+
+    # MariaDB저장(제목, 리뷰, 평점, 작성자, 작성일자)
+    data = {
+        "title": movie_title,
+        "review": review_content,
+        "score": review_score,
+        "writer": review_writer,
+        "reg_date": review_date
+    }
+    add_review(data)
