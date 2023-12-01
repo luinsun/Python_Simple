14e76178d81685d1d68b461f54a975cf4f6fb455
luinsun <135595841+luinsun@users.noreply.github.com>
2023-12-01 13:25:35 +0900

    Add files via upload

--- Python_Simple-main/project_daum_movie/main.py
+++ Python_Simple-main/project_daum_movie/main.py
@@ -1,3 +1,10 @@
+# TODO: 1.스케줄링
+#       2.리뷰 중복 체크(중복 수집X)
+#       3.DB에 저장된 데이터 Exel 다운로드
+#       4.DB에 저장된 데이터 -> 텍스트 분석
+#       5.DB에 저장된 데이터 -> WordCloud 시각화
+
+
 from collect.collect_daum_movie_review import review_collector
 
 
